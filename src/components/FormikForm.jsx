39d23a310c097b26a96e16fc6cc8c5144d5b5956
Denis Duarte <duarte.denis11@gmail.com>
2024-04-13 18:09:09 -0400

    Cleaner checkboxes

--- src/components/FormikForm.jsx
+++ src/components/FormikForm.jsx
@@ -8,10 +8,20 @@ const MasterCheckBox = (props) => {
 
     return (
         <Form.Check 
-        label={"Master Check Box"}
         {...field}
         {...props}
         onChange={() => {checked ? setFieldValue("members", []) : setFieldValue("members", ["1", "2", "3", "4"])}}
+        />
+    )
+}
+
+const NormalCheckBox = (props) => {
+    const [field] = useField(props);
+
+    return (
+        <Form.Check 
+        {...field}
+        {...props}
         />
     )
 }
@@ -28,19 +38,11 @@ const FormikFormComponent = memo(() => {
             {({values, setFieldValue}) => (
                 <div className='form-container'>
                 <FormikForm>
-                        <Field name="members" type="checkbox" value="0" checked={JSON.stringify(optionValues) === JSON.stringify(values.members)} as={MasterCheckBox} setFieldValue={setFieldValue} />
-                    <label>Checkbox 1
-                        <Field name="members" type="checkbox" value="1"/>
-                    </label>
-                    <label>Checkbox 2
-                        <Field name="members" type="checkbox" value="2"/>
-                    </label>
-                    <label>Checkbox 3
-                        <Field name="members" type="checkbox" value="3"/>
-                    </label>
-                    <label>Checkbox 4
-                        <Field name="members" type="checkbox" value="4"/>
-                    </label>
+                        <Field name="members" type="checkbox" value="0" checked={JSON.stringify(optionValues) === JSON.stringify(values.members)} as={MasterCheckBox} setFieldValue={setFieldValue} label={"Master Check Box"} />
+                        <Field name="members" type="checkbox" value="1" label={"Checkbox 1"} as={NormalCheckBox}/>
+                        <Field name="members" type="checkbox" value="2" label={"Checkbox 2"} as={NormalCheckBox}/>
+                        <Field name="members" type="checkbox" value="3" label={"Checkbox 3"} as={NormalCheckBox}/>
+                        <Field name="members" type="checkbox" value="4" label={"Checkbox 4"} as={NormalCheckBox}/>
                 </FormikForm>
                 <pre>{`values: ${JSON.stringify(values, null, 2)}`}</pre>
                 </div>
